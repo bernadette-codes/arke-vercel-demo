648e4d95b68c4bbdca4d546a67f9981236ac24da
Bernadette-arke <bernadette@englemaninvest.com>
2021-05-06 07:55:12 -0400

    Update content

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import React, {  useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import axios from "axios";
 
 export default function Home() {
   const [message, setMessage] = useState("Vercel & Next.js Demo");
   const [memeImage, setmemeImage] = useState("https://i.imgur.com/qcwCIUL.jpg");
+  const [quoteContent, setQuoteContent] = useState(null);
+  const [showQuote, setShowQuote] = useState(false);
 
   async function getMessage() {
     const res = await fetch("/api/message");
@@ -24,36 +26,76 @@ export default function Home() {
     setmemeImage(parsedData.image);
   }
 
+  useEffect(() => {
+    if (showQuote) {
+      axios
+        .get("https://api.quotable.io/random")
+        .then((response) => {
+          setQuoteContent(response.data.content);
+        })
+        .catch((error) => {
+          setQuoteContent(error);
+        });
+      setShowQuote(false);
+    }
+  }, [showQuote]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Vercel with Next.js Demo</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.topic}>
+          <h1 className={styles.title}>Vercel</h1>
+          <ul className={styles.intro}>
+            <li>What is Vercel?</li>
+            <li>Features of Vercel</li>
+            <li>Platform tour</li>
+            <li>Demo</li>
+          </ul>
+        </div>
+
+        <div className={styles.topic}>
+          <h2 className={styles.subHeader}>What is Vercel?</h2>
+          <ul className={styles.context}>
+            <li>Deployment platform for static sites</li>
+            <li>Host websites and web services that deploy instantly and scale automatically – all without any configuration</li>
+            <li>Builds your project when you trigger a deploy</li>
+            <li>Ddeployment and collaboration platform</li>
+          </ul>
+        </div>
+
+        <div className={styles.topic}>
+          <h2 className={styles.subHeader}>Features of Vercel</h2>
+          <ul className={styles.context}>
+            <li>Add custom domain</li>
+            <li>Environment: Production, Preview and Development</li>
+            <li>Option to define Environment Variables specific to the environment</li>
+            <li>Can deploy Serverless Functions to handle user authentication, form submission, database queries, custom slack commands, and more</li>
+            <li>Analytics that collects metrics</li> 
+            <li>Logging options</li>
+          </ul>
+        </div>  
+
+        <div className={styles.topic}>
+          <h2 onClick={getMessage} className={styles.demoText}>{message}</h2>
+
+          <img src={memeImage} onClick={getNewMeme} className={styles.meme} />
+
+          <button onClick={() => setShowQuote(true)}  className={styles.button}>Get Quote</button>
+
+          <p className={styles.quote}>{quoteContent}</p>
+        </div>
 
 
-        <h2 onClick={getMessage}>{message}</h2>
-
-        <img src={memeImage} onClick={getNewMeme}/>
 
 
 
 
 
-
-
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
+        {/* <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -82,18 +124,8 @@ export default function Home() {
             </p>
           </a>
         </div>
+       */}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
